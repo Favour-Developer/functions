25e3905af04ad5618eac3e69292410bff2e436a5
ritik2409 <guptaritik97@gmail.com>
2020-10-09 14:00:18 +0530

    New request notification

--- functions/index.js
+++ functions/index.js
@@ -3,24 +3,49 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   //   functions.logger.info("Hello logs!", {structuredData: true});
-//   initialise(d);
-// //  users.child("temp").set("Working fine..");
-// // users.child()
-// // processref.once('value').then(function(snapshot){
-// //   snapshot.forEach(function(child){
-// //     console.log("uid", child.val().favourerUID)
-// //   })
-// // })
-// var uid = "6oruSdSjhwZppsUeRt4EXagwrQ23"
-// const payload = {
-//   "data":{
-//     "title" : "Reminder",
-//     "body" : "Complete the accepted request now!",
-//     "sented": uid
-//   }
-// };
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  tokenref = db.ref('temp/Tokens');
+  return tokenref.once("value").then(function(snapshot){
+    snapshot.forEach(function(child){
+      // if(child.val().token!=="ePX-M1UyRhichrPXNATHNi:APA91bHUdy5BKfA4meAdpFPj5Yp1o-ghOwJCF7wLrAu_lTVkEMh1tOIfHWItRfxtHKD0MV9kFui0olnWDcx-hmOSQTTdbIooCCXQctkBPdbZobnKCxiLCH-fu9VAf3dPqmGc6D4161mV")
+      // tokens.push(child.val().token);
+      var uid = child.key;
+      console.log(uid)
+      const payload = {
+        "data":{
+          "title" : "Reminder",
+          "body" : "Complete the accepted request now!",
+          "sented": uid
+        }
+      };
+      admin.messaging().sendToDevice(child.val().token, payload).then(function(temp){
+             return console.log(temp);
+           }).catch(function(err){
+            return console.log(err);
+          });
+    });
+    return response.status(200).send("success");
+  });
+    // console.log(tokens)
+    //  admin.messaging().sendToDevice(result.val(), payload).then(function(temp){
+//      return response.send(temp);
+//    }).catch(function(err){
+//      return response.send(err);
+//    });
+//    return null;
+//  })
+ });
+
+  //   functions.logger.info("Hello logs!", {structuredData: true});
+  // initialise(d);
+//  users.child("temp").set("Working fine..");
+// users.child()
+// processref.once('value').then(function(snapshot){
+//   snapshot.forEach(function(child){
+//     console.log("uid", child.val().favourerUID)
+//   })
+// })
+
 
 // return db.ref(`${root}/Tokens/${uid}/token`).once('value').then(function(result){
 //    console.log(result.val());
@@ -183,3 +208,34 @@ exports.updatePassword = functions.https.onRequest((request, response) => {
         });
         return null;
   });
+
+exports.sendtoAll = functions.database.ref('/d1/requests/{requestID}').onCreate((change, context)=>{
+  var newreq = change.val();
+  // console.log(newreq);
+  var notuid = newreq.userUid;
+  var mbody = newreq.person_name + " has raised a favour request, help out now!"; 
+  console.log(mbody);
+  tokenref = db.ref('d1/Tokens');
+  return tokenref.once("value").then(function(snapshot){
+    snapshot.forEach(function(child){
+      // if(child.val().token!=="ePX-M1UyRhichrPXNATHNi:APA91bHUdy5BKfA4meAdpFPj5Yp1o-ghOwJCF7wLrAu_lTVkEMh1tOIfHWItRfxtHKD0MV9kFui0olnWDcx-hmOSQTTdbIooCCXQctkBPdbZobnKCxiLCH-fu9VAf3dPqmGc6D4161mV")
+      // tokens.push(child.val().token);
+      var uid = child.key;
+      if(uid === notuid) return;
+      // console.log(uid)
+      const payload = {
+        "data":{
+          "title" : "New Favour Request",
+          "body" : mbody,
+          "sented": uid
+        }
+      };
+      admin.messaging().sendToDevice(child.val().token, payload).then(function(temp){
+             return console.log("Success to", uid);
+           }).catch(function(err){
+            return console.log(err);
+          });
+    });
+    return null;
+  });
+});
